820ae0875bfc65655519889420deaf67fd70b2ae
AtsuyaMorishita <mori.a0426@gmail.com>
2023-05-21 17:13:59 +0900

    プロフィール文作成

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -37,21 +37,43 @@ export default function Profile() {
             <ProfileItem>
               <ProfileInfoTitle>SKILL</ProfileInfoTitle>
               <ProfileInfoText>
-                簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。
+                <ProfileInfoTextSubTitle>
+                  【言語 / フレームワーク】
+                </ProfileInfoTextSubTitle>
+                HTML（EJS） / CSS（SCSS） / Javascript（jQuery） / TypeScript /
+                React（Next.js）
+                <ProfileInfoTextSubTitle>【その他】</ProfileInfoTextSubTitle>
+                WordPress / microCMS / Shopify / Git / Github / Gulp / WebPack /
+                Vite / Firebase(CloudFirestore , Authentication) / Vercel /
+                Netlify
               </ProfileInfoText>
             </ProfileItem>
 
             <ProfileItem>
               <ProfileInfoTitle>HISTORY</ProfileInfoTitle>
               <ProfileInfoText>
-                簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。
+                愛知県の私立大学を卒業後、商業(ショッピングモール)デベロッパーの業界にて、テナントの売上管理・改善提案や契約関連業務などに従事。
+                <br />
+                在職中は宅地建物取引士の資格も取得。
+                <br />
+                一方でWeb制作の仕事に興味を持ち、独学でのコーディングの学習を得て現在の会社に転職。
+                <br />
+                約2年間コーダーとしてECサイトやWebサイトのコーディング/実装を行う。
+                <br />
+                現在はNext.jsなどモダンな技術やアプリ開発に興味を持ち、都内でフロントエンドエンジニアになるため転職活動中。
               </ProfileInfoText>
             </ProfileItem>
 
             <ProfileItem>
               <ProfileInfoTitle>HOBBY</ProfileInfoTitle>
               <ProfileInfoText>
-                簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。簡単なプロフィール文が入ります。
+                スポーツをするのも見るのも好きです。特に小中野球をやっていたのでプロ野球が好きで、WBCは有給使って見るほど熱狂していました⚾️笑
+                <br />
+                最近は週末テニススクールに通っていて、初挑戦の競技ですがどハマりしています🎾
+                <br />
+                また海外旅行も好きで、学生時代は約1ヶ月東南アジアにバックパッカーをしたり、一人で台湾に行ったりしていました。(社会人になって行けてませんが、、)
+                <br />
+                好奇心旺盛な性格なので、基本的に新しいことをしたり、新しい場所に行ったりすることは全般好きです！
               </ProfileInfoText>
             </ProfileItem>
           </ProfileList>
@@ -126,9 +148,27 @@ const ProfileInfoTitle = styled.span`
   background-color: ${COLOR.SECONDARY};
   padding: 2px 20px;
   font-weight: bold;
+  ${mediaQuery[0]} {
+    font-size: 1.8rem;
+  }
 `;
 
 const ProfileInfoText = styled.p`
   font-size: 1.4rem;
   margin-top: 10px;
+  ${mediaQuery[0]} {
+    font-size: 1.6rem;
+  }
+`;
+
+const ProfileInfoTextSubTitle = styled.span`
+  display: block;
+  font-weight: bold;
+  font-size: 1.4rem;
+  &:not(:first-of-type) {
+    margin-top: 10px;
+  }
+  ${mediaQuery[0]} {
+    font-size: 1.6rem;
+  }
 `;
